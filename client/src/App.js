37228decf0fe2db455cd7c1d2dd868d51651836f
mie6996 <nguyenxuanphat060301@gmail.com>
2022-04-13 07:39:55 +0700

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Landing from './components/auth/layout/Landing'
+import Landing from './components/layout/Landing'
 import Auth from "./views/Auth";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route exact path='/login' element={<Auth authRoute='login'/>}/>
-        <Route exact path='/register' element={<Auth authRoute='register'/>} />
+        <Route exact path='/login' element={<Auth authRoute='login' />} />
+        <Route exact path='/register' element={<Auth authRoute='register' />} />
       </Routes>
     </Router>
   );
