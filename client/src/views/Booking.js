7441a7bf916d730eb142c5aa7b0c31c4c3d308f4
tuthieu <dangtuthieu@gmail.com>
2022-04-16 18:42:45 +0700

    Booking testing

--- client/src/views/Booking.js
+++ client/src/views/Booking.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import { useContext, useState, useEffect } from "react";
+import { BookingsContext } from "../contexts/BookingsContext";
+import Spinner from "react-bootstrap/esm/Spinner";
+import { useNavigate } from "react-router-dom";
+import BootstrapTable from "react-bootstrap-table-next";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-const Booking = () => {
+const columns = [
+  {
+    dataField: "id",
+    text: "#",
+    headerStyle: (colum, colIndex) => {
+      return { backgroundColor: '#defffd', width: '20%', textAlign: 'center' };
+    }
+  },
+  {
+    dataField: "name",
+    text: "Product Name",
+  },
+  {
+    dataField: "value",
+    text: "Product value",
+  },
+];
+
+const Bookings = () => {
+  const {
+    bookingsState: { bookingsLoading, bookings },
+    getBookings,
+  } = useContext(BookingsContext);
+
+  useEffect(() => {
+    if (bookings.length === 0) {
+      getBookings();
+    }
+  }, []);
+
+  const navigate = useNavigate();
+
+  if (bookingsLoading) {
+    return (
+      <div className="spinner-container">
+        <Spinner animation="border" variant="danger" />
+      </div>
+    );
+  }
+
   return (
-    <div>Booking</div>
-  )
-}
-
-export default Booking
+    <>
+      <div className="container-fluid my-1 d-flex justify-content-between">
+        <div>
+          <button
+            className="btn btn-info"
+            onClick={() => {
+              navigate("/system-management");
+            }}
+          >
+            Trở lại
+          </button>
+        </div>
+        <button className="btn btn-info">Thêm Đặt phòng</button>
+      </div>
+      <div className="container" style={{ marginTop: 50 }}>
+        <BootstrapTable
+          striped
+          hover
+          keyField="id"
+          data={bookings}
+          columns={columns}
+        />
+      </div>
+    </>
+  );
+};
+export default Bookings;
